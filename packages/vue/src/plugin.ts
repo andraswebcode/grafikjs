62cd39282ae1ef14ff6ac2b886e2dd48ebcd1302
andraswebcode <andras.tovishati@gmail.com>
2024-07-17 14:39:03 +0200

    Made some changes

--- packages/vue/src/plugin.ts
+++ packages/vue/src/plugin.ts
@@ -4,6 +4,7 @@ const createGrafik = (opt?) => ({
 	install(app, options = opt) {
 		const canvas = new Canvas(options);
 		app.provide('canvas', canvas);
+		app.grafikCanvas = canvas;
 		if (process.env.NODE_ENV === 'development') {
 			// @ts-ignore
 			window.grafikCanvas = canvas;
